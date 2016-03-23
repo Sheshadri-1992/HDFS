c99fee529f8e89790b5a4ecbc843811a12763e2c
Sheshadri-1992 <sheshadri.madhu@gmail.com>
2016-03-23 20:07:19 +0530

    DataNode Driver

--- src/com/hdfs/datanode/DataNodeDriver.java
+++ src/com/hdfs/datanode/DataNodeDriver.java
@@ -1,10 +1,51 @@
 package com.hdfs.datanode;
 
-public class DataNodeDriver {
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
 
+
+
+public class DataNodeDriver implements IDataNode {
+
+	/**Interface methods start here **/
+	public byte[] readBlock(byte[] inp) throws RemoteException {
+		// TODO Auto-generated method stub
+		
+		return null;
+	}
+
+
+	public byte[] writeBlock(byte[] inp) throws RemoteException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**Interface methods end here **/
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	}
+		System.out.println("Datanode "+ args[0]);
+		/**Need an argument from command line to uniquely identify the data Node **/
+		DataNodeDriver dataDriverObj = new DataNodeDriver();
+		
+		Registry register = null;
 
+		//Registering an object in java RMI environment
+		try
+		{
+			IDataNode dataStub = (IDataNode) UnicastRemoteObject.exportObject(dataDriverObj);
+			register = LocateRegistry.createRegistry(8000);
+			register.rebind("Datanode1",dataStub);
+		}
+		catch(RemoteException e)
+		{
+			System.out.println("Remote Exception Caught DataNodeDriverClas  method:main");
+		}
+		
+	}
+	
 }
