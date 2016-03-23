4cf6050f8c75a796fb899053bf6b06a3bf68030f
Shweta Verma <cool.shwetav@gmail.com>
2016-03-24 01:38:27 +0530

    Updated constants

--- src/com/hdfs/miscl/Constants.java
+++ src/com/hdfs/miscl/Constants.java
@@ -4,7 +4,12 @@ public final class Constants {
 
 
 	public static final String  NAME_NODE = "NameNode";  //added by shweta
-	public static final String  NAME_NODE_IP = "127.0.0.1";  //added by shweta
+	public static final String  NAME_NODE_IP = "10.0.0.1";  //added by shweta
 	public static final String DATA_NODE_ID = "Datanode";//constant by sheshadri
+	
+	
+	public static final int STATUS_SUCCESS = 1;//constant by shweta
+	public static final int STATUS_NOT_FOUND = -1;//constant by shweta
+	
 	
 }
