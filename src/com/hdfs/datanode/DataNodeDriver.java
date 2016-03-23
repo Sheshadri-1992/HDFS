99339ef31a1c50209e071d4baa74d5483b9eb237
Sheshadri-1992 <sheshadri.madhu@gmail.com>
2016-03-23 20:30:49 +0530

    datanodedriver file

--- src/com/hdfs/datanode/DataNodeDriver.java
+++ src/com/hdfs/datanode/DataNodeDriver.java
@@ -1,10 +1,14 @@
 package com.hdfs.datanode;
 
+import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 
+import com.hdfs.namenode.INameNode;
+
+import static com.hdfs.miscl.Constants.*;
 
 
 public class DataNodeDriver implements IDataNode {
@@ -32,20 +36,37 @@ public class DataNodeDriver implements IDataNode {
 		/**Need an argument from command line to uniquely identify the data Node **/
 		DataNodeDriver dataDriverObj = new DataNodeDriver();
 		
+		
 		Registry register = null;
 
 		//Registering an object in java RMI environment
 		try
 		{
-			IDataNode dataStub = (IDataNode) UnicastRemoteObject.exportObject(dataDriverObj);
+			IDataNode dataStub = (IDataNode) UnicastRemoteObject.exportObject(dataDriverObj,0);
 			register = LocateRegistry.createRegistry(8000);
-			register.rebind("Datanode1",dataStub);
+			register.rebind(DATA_NODE_ID+args[1],dataStub);
 		}
 		catch(RemoteException e)
 		{
 			System.out.println("Remote Exception Caught DataNodeDriverClas  method:main");
 		}
 		
+		callMethod();
+		
+	}
+
+
+	private static void callMethod() {
+		// TODO Auto-generated method stub
+		try {
+			Registry register = LocateRegistry.getRegistry(NAME_NODE_IP,Registry.REGISTRY_PORT);
+			INameNode nameNodeStub = (INameNode)register.lookup("NameNode");
+			
+			
+		} catch (RemoteException | NotBoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 }
--- src/com/hdfs/miscl/Constants.java
+++ src/com/hdfs/miscl/Constants.java
@@ -3,5 +3,6 @@ package com.hdfs.miscl;
 public final class Constants {
 
 	public static final String  NAME_NODE_IP = "127.0.0.1";
+	public static final String DATA_NODE_ID = "Datanode";//constant by sheshadri
 	
 }
